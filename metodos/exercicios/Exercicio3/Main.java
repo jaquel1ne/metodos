bf1336f9030bde6c9947ca1f04ccb15c906cbeda
jaquel1ne <jaquilinas@gmail.com>
2023-03-17 23:01:08 -0300

    commit inicial

---
+++ metodos/exercicios/Exercicio3/Main.java
@@ -0,0 +1,22 @@
+package Exercicio3;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Retornos
+        System.out.println("Exercícios Retornos");
+
+        double areaQuadrado = Quadrilatero.area(3);
+        System.out.println("Área do quadrado: " + areaQuadrado);
+
+        double areaRetangulo = Quadrilatero.area(5,5);
+        System.out.println("Área do retângulo: " + areaRetangulo);
+
+        double areaTrapezio = Quadrilatero.area(7,8,9);
+        System.out.println("Área do trapézio: " + areaTrapezio);
+
+        
+    }
+    
+}
